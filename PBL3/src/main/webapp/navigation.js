55351a0eb89897931f9bb89aaeff472663d8b5e3
QuantumCat <tranhoangphuchuy04@gmail.com>
2024-05-26 17:35:00 +0700

    Fix: sidebar with toggle for owner's sidebar

--- PBL3/src/main/webapp/navigation.js
+++ PBL3/src/main/webapp/navigation.js
@@ -1,7 +1,28 @@
-$(".menu > ul > li").click(function (e) {
-  $(this).addClass("active");
-  $(this).siblings().removeClass("active");
+$(document).ready(function () {
+  $(".productSidebar > a").on("click", function (e) {
+    e.preventDefault();
+    $(this).siblings(".submenu").slideToggle();
+  });
+
+  $(".menu > ul > li").click(function (e) {
+    $(this).addClass("active");
+    $(this).siblings().removeClass("active");
+
+    if (!$(this).hasClass("productSidebar")) {
+      $(".submenu > li").removeClass("active");
+    }
+  });
+
+  $(".submenu > li").click(function (e) {
+    $(this).addClass("active");
+    $(this).siblings().removeClass("active");
+  });
 });
+
+// $(".menu > ul > li").click(function (e) {
+//   $(this).addClass("active");
+//   $(this).siblings().removeClass("active");
+// });
 
 const HeaderBreadcrumbLink = document.getElementById("HeaderBreadcrumbLink");
 
